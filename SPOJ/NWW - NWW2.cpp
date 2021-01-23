addd47a40ef9ee318b40273fc020cd0425e038f5
Marcin Wojdat <wojdatm98@gmail.com>
2021-01-23 19:31:57 +0100

    Solves NWW - NWW2 (SPOJ) task

---
+++ SPOJ/NWW - NWW2.cpp
@@ -0,0 +1,148 @@
+// Task: NWW - NWW2 (SPOJ)
+// URL: https://pl.spoj.com/problems/NWW/
+// Tags: [LCM], [GCD]
+// Author: Marcin Wojdat
+// Date: 23.01.2021
+// Algorithms from: https://github.com/Rubix98/algorithms
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Biblioteczki
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <numeric>
+using namespace std;
+// Typy proste
+typedef unsigned int        UI;
+typedef long long           LL;
+typedef unsigned long long  ULL;
+// Pary
+typedef pair<int, int>  PI;
+#define MP              make_pair
+#define ST              first
+#define ND              second
+// Vectory
+typedef vector<int>  VI;
+typedef vector<bool> VB;
+typedef vector<double> VD;
+typedef vector<string> VS;
+typedef vector<VI>   VVI;
+typedef vector<PI>   VPI;
+#define VT           vector<T>
+#define VVT          vector<VT >
+#define PB           push_back
+#define SIZE(v)      (int(v.size()))
+#define ALL(v)       v.begin(), v.end()
+// Pętle
+#define LOOP(i, a, b)  for (int i = (a); i < (int)(b); ++i)
+#define FOREACH(x, v)  for (auto &x: v)
+#define REP(i, n)      LOOP(i, 0, n)
+// Stałe
+const int INF = 1e9+9;
+const double EPS = 1e-9;
+// Grafy
+#define ET   Edge<T>
+#define GT   Graph<T>
+#define VE   vector<ET*>
+#define VVE  vector<VE > 
+#define PVTVE pair<VT, VE >
+// Geometria
+#define VP vector<Point>
+#define ISZERO(x) abs(x) < EPS
+// Inne
+#define MOD(a, m) (a % m + m) % m
+#define TEMPL template <typename T>
+#define NP nullptr
+#define BETWEEN(x, a, b) (a) <= x && x <= (b) || (b) <= x && x <= (a)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Biblioteczki
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <bitset>
+#include <stack>
+#include <queue>
+
+// Vectory
+typedef vector<UI>      VUI;
+typedef vector<LL>      VLL;
+typedef vector<ULL>     VULL;
+
+// Pary
+typedef pair<double, double>  PD;
+typedef pair<string, string>  PS;
+typedef pair<UI, UI>          PUI;
+typedef pair<LL, LL>          PLL;
+typedef pair<ULL, ULL>        PULL;
+
+// Pętle
+#define DLOOP(i, a, b)  for (int i = (a); i <= (b); ++i)
+#define RLOOP(i, a, b)  for (int i = (a); i >= (b); --i)
+#define TESTS(t)        int t; cin >> t; REP(i, t) 
+
+// Stałe
+const LL LLINF = 1e18+9;
+
+// Konwersja char na int i odwrotnie
+#define CTOI(c) (int(c))
+#define ITOC(x) (char(x))
+
+// I/O
+#define FASTIO()  ios_base::sync_with_stdio(0); cin.tie(NULL);
+TEMPL void printTab(VT &v) {
+    cout << "[";
+    REP(i, v.size())
+        cout << (i ? ", " : "") << v[i];
+    cout << "]\n";
+}
+TEMPL void readTab(VT &v) {
+    int n;
+    cin >> n;
+    v.resize(n);
+    FOREACH(x, v) cin >> x;
+}
+
+// Debugowanie
+#ifdef DEBUGGING
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#else
+#define DEBUG(x) 
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ULL gcd(ULL a, ULL b) {
+    if (a < b) swap(a, b);
+    return b ? gcd(b, a % b) : a;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+ULL lcm(ULL a, ULL b) {
+    return (a || b) ? a / gcd(a, b) * b : 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+    FASTIO();
+    TESTS(t) {
+        VULL tab;
+        readTab(tab);
+        ULL res = tab[0];
+        FOREACH(x, tab) {
+            res = lcm(res, x);
+        }
+        cout << res << endl;
+    }
+}
+
