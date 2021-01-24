fb6a70eac4fe2865c4accba323d25db7539f1b36
Marcin Wojdat <wojdatm98@gmail.com>
2021-01-24 13:33:14 +0100

    Solves DYZIO2 - Dyzio task

---
+++ SPOJ/DYZIO2 - Dyzio.cpp
@@ -0,0 +1,157 @@
+// Task: DYZIO2 - Dyzio (SPOJ)
+// URL: https://pl.spoj.com/problems/DYZIO2/
+// Author: Marcin Wojdat
+// Date: 24.01.2021
+// Algorithms from: https://github.com/Rubix98/algorithms
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Biblioteczki
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <numeric>
+using namespace std;
+// Typy proste
+typedef unsigned int        UI;
+typedef long long           LL;
+typedef unsigned long long  ULL;
+// Pary
+typedef pair<int, int>  PI;
+#define MP              make_pair
+#define ST              first
+#define ND              second
+// Vectory
+typedef vector<int>  VI;
+typedef vector<bool> VB;
+typedef vector<double> VD;
+typedef vector<string> VS;
+typedef vector<VI>   VVI;
+typedef vector<PI>   VPI;
+#define VT           vector<T>
+#define VVT          vector<VT >
+#define PB           push_back
+#define SIZE(v)      (int(v.size()))
+#define ALL(v)       v.begin(), v.end()
+// Pętle
+#define LOOP(i, a, b)  for (int i = (a); i < (int)(b); ++i)
+#define FOREACH(x, v)  for (auto x: v)
+#define REP(i, n)      LOOP(i, 0, n)
+// Stałe
+const int INF = 1e9+9;
+const double EPS = 1e-9;
+// Grafy
+#define ET   Edge<T>
+#define GT   Graph<T>
+#define VE   vector<ET*>
+#define VVE  vector<VE > 
+#define PVTVE pair<VT, VE >
+// Geometria
+#define VP vector<Point>
+#define ISZERO(x) abs(x) < EPS
+// Inne
+#define MOD(a, m) (a % m + m) % m
+#define TEMPL template <typename T>
+#define NP nullptr
+#define BETWEEN(x, a, b) (a) <= x && x <= (b) || (b) <= x && x <= (a)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Biblioteczki
+#include <string>
+#include <map>
+#include <set>
+#include <list>
+#include <bitset>
+#include <stack>
+#include <queue>
+
+// Vectory
+typedef vector<UI>      VUI;
+typedef vector<LL>      VLL;
+typedef vector<ULL>     VULL;
+
+// Pary
+typedef pair<double, double>  PD;
+typedef pair<string, string>  PS;
+typedef pair<UI, UI>          PUI;
+typedef pair<LL, LL>          PLL;
+typedef pair<ULL, ULL>        PULL;
+
+// Pętle
+#define DLOOP(i, a, b)  for (int i = (a); i <= (b); ++i)
+#define RLOOP(i, a, b)  for (int i = (a); i >= (b); --i)
+#define TESTS(t)        int t; cin >> t; REP(i, t) 
+
+// Stałe
+const LL LLINF = 1e18+9;
+
+// Konwersja char na int i odwrotnie
+#define CTOI(c) (int(c))
+#define ITOC(x) (char(x))
+
+// I/O
+#define FASTIO()  ios_base::sync_with_stdio(0); cin.tie(NULL);
+TEMPL void printTab(VT &v) {
+    cout << "[";
+    REP(i, v.size())
+        cout << (i ? ", " : "") << v[i];
+    cout << "]\n";
+}
+TEMPL void readTab(VT &v) {
+    int n;
+    cin >> n;
+    v.resize(n);
+    REP(i, n) cin >> v[i];
+}
+
+// Debugowanie
+#ifdef DEBUGGING
+#define DEBUG(x) cout << #x << ": " << x << endl;
+#else
+#define DEBUG(x) 
+#endif
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+VB eratosthenes(int n) {
+    VB res = VB(n, 1);
+    res[0] = res[1] = 0;
+    int sq = sqrt(n);
+    LOOP(i, 2, sq+1)
+        if (res[i]) 
+            for (int j = i+i; j < n; j += i)
+                res[j] = 0;
+    return res;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T, typename T2 = T> struct PrefixSums {
+    vector<T2> sumTab;
+    PrefixSums(VT &tab = VT()) {
+        sumTab.PB(0);
+        FOREACH(x, tab) sumTab.PB(sumTab.back() + x);
+    }
+    
+    T2 sum(int a, int b) {
+        return a < b ? sumTab[b] - sumTab[a] : 0;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main() {
+    FASTIO();
+    VB isPrime = eratosthenes(1e6+1);
+    PrefixSums<bool, int> ps(isPrime);
+    TESTS(t) {
+        int a, b;
+        cin >> a >> b;
+        cout << ps.sum(a, b+1) << endl;
+    }
+}
